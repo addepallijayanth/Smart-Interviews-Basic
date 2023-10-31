4fad96916385637aad0fc8f828990f4e75f63820
Jayanth Addepalli <140036441+addepallijayanth@users.noreply.github.com>
2023-10-31 21:22:09 +0530

    Create cubes sum.c

---
+++ cubes sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+long long n,i,sum=0;
+    scanf("%lld",&n);
+    for(i=1;i<=n;i++)
+    {
+        sum=sum+(i*i*i);
+    }
+    printf("%lld",sum);
+}
